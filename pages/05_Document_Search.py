3100b8434cc17859cf9fcb3a6da72c3ff49eb85a
kiwon-crema <kiwon.yun@cre.ma>
2023-10-23 09:22:33 +0900

    document search added

---
+++ pages/05_Document_Search.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import os
+# from utils import set_openai_api_key
+
+from langchain.document_loaders import PyPDFLoader
+
+def file_selector(folder_path='.'):
+    files = [f for f in os.listdir(
+        folder_path) if os.path.isfile(f) and f.endswith(".pdf")]
+    selected_filename = st.selectbox('Select a file', files)
+    return os.path.join(folder_path, selected_filename)
+
+if __name__ == "__main__":
+    # set_openai_api_key()
+    filepath = file_selector('data')
+
+    if filepath:
+        filename_full = filepath
+        filename = filename_full.split('.')[0]
+
+        with st.spinner('Loading...'):
+            # Step 1: LOAD: document loading (txt, web, pdf)
+            raw_documents = PyPDFLoader(filename_full).load_and_split()
